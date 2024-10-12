bfa68eb0e41931d13c6de9890ea01509d9f9f175
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2024-10-12 23:04:21 +0200

    added types for frontend

--- client/src/lib/types.ts
+++ client/src/lib/types.ts
@@ -2,3 +2,71 @@ export interface Admin {
   id: number;
   username: string;
 }
+
+export interface User {
+  id: number;
+  firstName: string;
+  secondName: string;
+  email: string;
+  country?: string;
+  address?: string;
+  city?: string;
+  pinCode?: string;
+  phoneNumber?: string;
+  wishList?: Product[];
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  shortDescription: string;
+  fullDescription: string;
+  material: string;
+  images: string[];
+  color: string[];
+  sizes: string[];
+  gender: string;
+  category: string;
+  regularPrice: number;
+  discountPrice?: number;
+  totalOrders: number;
+  wishlistedBy: User[];
+}
+
+export interface Order {
+  id: number;
+  user: User;
+  userId: number;
+  orderProducts: OrderProduct[];
+  totalAmount: number;
+  paymentIntentId: string;
+  paymentStatus: string;
+  orderStatus: string;
+  shippingAddress?: string;
+  city?: string;
+  country?: string;
+  pincode?: string;
+  phoneNumber?: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface OrderProduct {
+  id: number;
+  product: Product;
+  productId: number;
+  quantity: number;
+  price: number;
+  order: Order;
+  orderId: number;
+}
+
+export interface Review {
+  id: number;
+  username: string;
+  rating: number;
+  text: string;
+  createdAt: string;
+  product: Product;
+  productId: number;
+}
